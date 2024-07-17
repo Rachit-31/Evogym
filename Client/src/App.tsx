a93355ca856fe4309153387bf2ca26406cf3f3c1
Rachit Jain <rachitjainjaipur@gmail.com>
2024-07-17 13:52:52 +0530

    half work done of navbar

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,9 +1,11 @@
+import Navbar from "./Scenes/navbar/Navbar"
+
 function App() {
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
+    <h1 className="app bg-gray-20">
+      <Navbar/>
     </h1>
     </>
   )
