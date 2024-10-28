d516645478cd6a8efe44e2fc0e2e1f71f00c3b61
Rachit Jain <rachitjainjaipur@gmail.com>
2024-10-28 20:16:28 +0530

    .

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,11 +1,20 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+//   },
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-  },
+  // resolve: {
+  //   alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  // },
 });
