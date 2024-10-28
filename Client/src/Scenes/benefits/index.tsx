b07e3d2fc9c57acc967aa071b44c07edfaf1bf64
Rachit Jain <rachitjainjaipur@gmail.com>
2024-10-28 18:10:16 +0530

    some improvments

--- Client/src/Scenes/benefits/index.tsx
+++ Client/src/Scenes/benefits/index.tsx
@@ -1,47 +1,54 @@
+import ActionButton from "@/shared/ActionButton";
+import HText from "@/shared/HText";
 import { BenefitType, SelectedPage } from "@/shared/types";
 import {
-    HomeModernIcon,
-    UserGroupIcon,
-    AcademicCapIcon,
-  } from "@heroicons/react/24/solid";
-  import { motion } from "framer-motion";
-  import HText from "@/shared/HText";
-import Benefit from "./Benefit";
+  HomeModernIcon,
+  UserGroupIcon,
+  AcademicCapIcon,
+} from "@heroicons/react/24/solid";
+import { motion } from "framer-motion";
 import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png";
-import ActionButton from "@/shared/ActionButton";
+import Benefit from "./Benefit";
 
-  const benefits: Array<BenefitType> = [
-    {
-        icon: <HomeModernIcon className="h-6 w-6"/>,
-        title: "State of the Art Facilities",
-        description:
+const benefits: Array<BenefitType> = [
+  {
+    icon: <HomeModernIcon className="h-6 w-6" />,
+    title: "State of the Art Facilities",
+    description:
       "Neque adipiscing amet amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et.",
-    },
-    {
-        icon: <UserGroupIcon className="h-6 w-6"/>,
-        title: "100's of Diverse Classes",
-        description:
+  },
+  {
+    icon: <UserGroupIcon className="h-6 w-6" />,
+    title: "100's of Diverse Classes",
+    description:
       "Eu ipsum id egestas risus tempus enim semper felis quis. Nec consectetur ac venenatis facilisi est. Eget ac turpis id.",
-    },
-    {
-        icon: <AcademicCapIcon className="h-6 w-6" />,
-        title: "Expert and Pro Trainers",
-        description:
-          "Fusce vestibulum aliquam ut cras. Nisl lectus egestas sapien nisl. Lacus at mi sit pellentesque. Congue parturient.",
-      },
-  ]
-type Props = {
-    setSelectedPage: (value: SelectedPage) => void;
-}
+  },
+  {
+    icon: <AcademicCapIcon className="h-6 w-6" />,
+    title: "Expert and Pro Trainers",
+    description:
+      "Fusce vestibulum aliquam ut cras. Nisl lectus egestas sapien nisl. Lacus at mi sit pellentesque. Congue parturient.",
+  },
+];
 
-const Benefits = ({setSelectedPage}: Props) => {
+const container = {
+  hidden: {},
+  visible: {
+    transition: { staggerChildren: 0.2 },
+  },
+};
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const Benefits = ({ setSelectedPage }: Props) => {
   return (
     <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
-
-    <motion.div
+      <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
       >
-        {/* header */}
+        {/* HEADER */}
         <motion.div
           className="md:my-5 md:w-3/5"
           initial="hidden"
@@ -53,30 +60,33 @@ const Benefits = ({setSelectedPage}: Props) => {
             visible: { opacity: 1, x: 0 },
           }}
         >
-                <HText>MORE THAN JUST GYM.</HText>
-                <p className="my-5 text-sm">We provide world class fitness equipment, trainers and classes to
+          <HText>MORE THAN JUST GYM.</HText>
+          <p className="my-5 text-sm">
+            We provide world class fitness equipment, trainers and classes to
             get you to your ultimate fitness goals with ease. We provide true
-            care into each and every member.</p>
+            care into each and every member.
+          </p>
         </motion.div>
 
-        {/* benefits */}
+        {/* BENEFITS */}
         <motion.div
           className="mt-5 items-center justify-between gap-8 md:flex"
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
-         //variants={container}
+          variants={container}
         >
-            {benefits.map((benefit: BenefitType) => (
-                <Benefit
-                key={benefit.title}
-                icon={benefit.icon}
-                title={benefit.title}
-                description={benefit.description}
-                setSelectedPage={setSelectedPage}
-                />
-            ))}
-    </motion.div>
+          {benefits.map((benefit: BenefitType) => (
+            <Benefit
+              key={benefit.title}
+              icon={benefit.icon}
+              title={benefit.title}
+              description={benefit.description}
+              setSelectedPage={setSelectedPage}
+            />
+          ))}
+        </motion.div>
+
         {/* GRAPHICS AND DESCRIPTION */}
         <div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
           {/* GRAPHIC */}
@@ -146,9 +156,9 @@ const Benefits = ({setSelectedPage}: Props) => {
             </div>
           </div>
         </div>
-    </motion.div>
+      </motion.div>
     </section>
-  )
-}
+  );
+};
 
-export default Benefits
+export default Benefits;
--- Client/src/Scenes/navbar/Link.tsx
+++ Client/src/Scenes/navbar/Link.tsx
@@ -1,26 +1,26 @@
-import AnchorLink from "react-anchor-link-smooth-scroll"
 import { SelectedPage } from "@/shared/types";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
-    page: string;
-    selectedPage: SelectedPage;
-    setSelectedPage: (value: SelectedPage) => void;
-  };
+  page: string;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
 const Link = ({ page, selectedPage, setSelectedPage }: Props) => {
-    const lowerCasePage = page.toLowerCase().replace(/ /g, "") as SelectedPage
+  const lowerCasePage = page.toLowerCase().replace(/ /g, "") as SelectedPage;
+
   return (
     <AnchorLink
-    className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""}
-    transition duration-500 hover:text-primary-300
-  `}
-  onClick={() => setSelectedPage(lowerCasePage)}
-    href={`#${lowerCasePage}`}
+      className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""}
+        transition duration-500 hover:text-primary-300
+      `}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
     >
-        {page}
+      {page}
     </AnchorLink>
-  )
-}
+  );
+};
 
-export default Link
-// 1:01:
+export default Link;
